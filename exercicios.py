0df91b4f61ec5da8e79d1950f23fba9c069aa19d
Gabriel1716 <130562661+Gabriel1716@users.noreply.github.com>
2024-05-22 08:03:37 -0300

    Add files via upload

---
+++ exercicios.py
@@ -0,0 +1,12 @@
+class Exercicios:
+    def __init__(self):
+        print("-")
+
+    def exercicio1(self,nome,idade,salario,cargo,turno,setor):
+        print("Meu nome é", nome, "tenho", idade, "anos," "ganho," "R$", salario, "meu cargo é", cargo, "trabalho no período", turno, "no setor", setor)
+    
+    def exercicio2(self,nomeEscola,estado,numeroProfessores,cidade,numeroMilitares,logradouro,numeroEndereco,numeroAlunos,colegioMilitar,disciplinas)
+        
+ex = exercicio()
+ex.exercicio1(nome = "Gustavo", idade = 14, salario = "1.980", cargo = auxiliar, turno = )
+
